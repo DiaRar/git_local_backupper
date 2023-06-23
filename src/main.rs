07a76f0922a8b8ce3f38d654e1f4c84f6d84cb69
DiaRar <cooleridcool@gmail.com>
2023-06-23 22:18:31 +0300

    "Empty"

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,38 @@
-use std::env::current_dir;
-use std::process::Command;
+use std::env::{current_dir, args};
+use std::path::Path;
+use std::process::{Command, Output};
+use std::io;
+
 fn get_current_dir() -> String {
     let current_dir = current_dir().unwrap();
     let current_dir = current_dir.to_str().unwrap();
     current_dir.to_string()
 }
-fn run_in_terminal(command: &str, cwd: &str) {
+fn run_in_terminal(command: &str, cwd: &str) -> io::Result<Output> {
     Command::new("cmd")
         .args(&["/C", command])
         .current_dir(cwd)
         .output()
-        .expect("failed to execute process");
+
 }
 fn main() {
-    let mut current_dir = get_current_dir();
-    run_in_terminal("git ", &current_dir);
-    current_dir[0] = 'D';
+    let current_dir = get_current_dir();
+    let mut destination_dir = current_dir.clone();
+    destination_dir.replace_range(0..1, "D");
+    match Path::new(&destination_dir).exists() {
+        true => println!("Destination directory exists"),
+        false => {
+            run_in_terminal(&format!("mkdir {}", &destination_dir), &current_dir).expect("Failed to create directory");
+        }
+    }
+    let commit_message = args().nth(1).unwrap_or("Empty".to_string());
+    let adding_dir = run_in_terminal(&format!("git remote add local {}", &destination_dir), &current_dir);
+    match adding_dir {
+        Ok(_) => println!("Remote added"),
+        Err(_) => println!("Remote already exists"),
+    }
+    run_in_terminal("git add .", &current_dir).expect("Failed to add files");
+    run_in_terminal(&format!("git commit -m \"{}\"", commit_message), &current_dir).expect("Failed to commit");
+    run_in_terminal("git init --base", &destination_dir).expect("Failed to push");
+    run_in_terminal("git push local master", &current_dir).expect("Failed to push");
 }
