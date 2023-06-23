28a31ca690ebf2c397bcf8df58f57dd9c0357f85
DiaRar <cooleridcool@gmail.com>
2023-06-23 22:58:33 +0300

    "Empty"

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::env::{current_dir, args};
 use std::path::Path;
 use std::process::{Command, Output};
 struct Dir {
-    dir: String,
+    current_dir: String,
     dir_name: String,
     parent: String,
 }
@@ -13,7 +13,7 @@ fn get_current_dir() -> Dir {
     let current_dir = current_dir.to_str().unwrap();
     
     Dir {
-        dir: current_dir.to_string(),
+        current_dir: current_dir.to_string(),
         dir_name: current_dir_name.to_str().unwrap().to_string(),
         parent: parent.to_str().unwrap().to_string(),
     }
@@ -31,7 +31,7 @@ fn add_and_commit(current_dir: &str, commit_message: &str) {
     let _git_commit = run_in_terminal(&format!("git commit -m \"{}\"", commit_message), &current_dir);
 }
 fn main() {
-    let Dir{dir: current_dir, dir_name, parent} = get_current_dir();
+    let Dir{current_dir, dir_name, parent} = get_current_dir();
     let mut destination_dir = parent.clone();
     destination_dir.replace_range(0..1, "D");
     destination_dir.push_str("\\git_backups\\");
@@ -55,15 +55,14 @@ fn main() {
     match git_push.status.success() {
         true => println!("Files pushed"),
         false => {
-            println!("Push Error \n {}", String::from_utf8_lossy(&git_push.stderr));
+            println!("Usually it's because there is another local lmao");
             run_in_terminal("git remote rm local", &current_dir);
             add_and_commit(&current_dir, &commit_message);
             run_in_terminal(&format!("git remote add local {}", &destination_dir), &current_dir);
             let push_retry = run_in_terminal("git push local master", &current_dir);
-            if !push_retry.status.success() {
-                panic!("Push Error \n {}", String::from_utf8_lossy(&push_retry.stderr));
-            } else {
-                println!("Files pushed");
+            match push_retry.status.success() {
+                true => println!("Files pushed"),
+                false => panic!("Push Error \n {}", String::from_utf8_lossy(&push_retry.stderr)),
             }
         },
     }
